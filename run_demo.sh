e338e3c8681534a9ddaa867381625ac5721b765e
Matthias Buhl <matthias.buhl@hensoldt-cyber.com>
2020-05-20 08:44:28 +0200

    [REM] Remove create tap section from run script since it is now in the container

--- run_demo.sh
+++ run_demo.sh
@@ -2,36 +2,15 @@
 
 #-------------------------------------------------------------------------------
 #
-# Script to run a demo using TAP devices
+# Script to run the IoT Demo
 #
 # Copyright (C) 2020, Hensoldt Cyber GmbH
 #
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
-function create_tap_device()
-{
-    # create TAP devices. Ethernet connection is required for this step!
-    ip tuntap add tap0 mode tap user $SUDO_USER
-    ip link add br0 type bridge
-    ip link set tap0 master br0
-    ip link set dev enp0s31f6 down
-    ip addr flush dev enp0s31f6
-    ip link set enp0s31f6 master br0
-    ip link set dev br0 up
-    ip link set tap0 up
-    ip link set dev enp0s31f6 up
-
-    iptables -A INPUT -i tap0 -j ACCEPT
-    iptables -A INPUT -i br0 -j ACCEPT
-    iptables -A FORWARD -i br0 -j ACCEPT
-
-    echo "created TAP0 device"
-}
-
-#-------------------------------------------------------------------------------
 USAGE_STRING="run_demo.sh <path-to-project-build> <path-to-proxy>\n
-This script runs demo applications that require TAP\n"
+This script runs the IoT Demo app\n"
 OPT_INTERACTIVE="-it"
 
 
@@ -61,12 +40,6 @@ if [ ! -f ${PROXY_PATH}/proxy_app ]; then
 fi
 
 shift 2
-
-# check if TAP device is already created and if not, set one up
-if [ ! -d "/sys/class/net/tap0/" ]; then
-  echo "TAP devices not created yet. Setting up TAP0..."
-  create_tap_device
-fi
 
 QEMU_PARAMS=(
     -machine xilinx-zynq-a9
