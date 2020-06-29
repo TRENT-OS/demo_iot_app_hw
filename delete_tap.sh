f141fae28b4efc5b2db9e66a9a7d718ee91c9eb1
Matthias Buhl <matthias.buhl@hensoldt-cyber.com>
2020-06-29 15:06:25 +0200

    [REM] Remove delete_tap.sh as it is not needed any longer

--- delete_tap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ip tuntap del dev tap0 mode tap
-ip tuntap del dev tap1 mode tap
-ip link delete enp0s31f6 type bridge
-ip link delete br0 type bridge
-ip link set dev enp0s31f6 up
-dhclient -v enp0s31f6
-
